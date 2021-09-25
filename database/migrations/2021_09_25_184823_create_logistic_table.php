f26f0c1beb4714361652eec1d8161eb1ba0576d3
Júlio Alves <julio.a.valenca@gmail.com>
2021-09-25 15:59:51 -0300

    implementacao de migrations adicionais

---
+++ database/migrations/2021_09_25_184823_create_logistic_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogisticTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logistic', function (Blueprint $table) {
+            $table->id();
+            $table->string('logistic_name');
+            $table->boolean('is_free');
+            $table->timestamp('estimated_shipping_fee');
+            $table->boolean('enabled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logistic');
+    }
+}
---
+++ database/migrations/2021_09_25_185327_create_attributes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributes', function (Blueprint $table) {
+            $table->string('attribute_name');
+            $table->boolean('is_mandatory');
+            $table->id('attribute_id');
+            $table->string('attribute_value');
+            $table->string('string_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attributes');
+    }
+}
