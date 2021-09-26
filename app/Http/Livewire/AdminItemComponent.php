410e505a30cdf5c50a1382cb722e787f62b8d5c8
Júlio Alves <julio.a.valenca@gmail.com>
2021-09-26 11:16:55 -0300

    adicao da view admin item

---
+++ app/Http/Livewire/AdminItemComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AdminItemComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin-item-component');
+    }
+}
